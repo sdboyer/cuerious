c2892bb2418c9de23644e3f1403673ed4a3da1c0
sam boyer <sdboyer@grafana.com>
2023-01-16 19:25:03 -0500

    cuerious: add testing framework

---
+++ all_test.go
@@ -0,0 +1,46 @@
+package cuerious_test
+
+import (
+	"fmt"
+	"testing"
+
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/cuecontext"
+	"cuelang.org/go/cue/format"
+	"github.com/sdboyer/cuerious"
+	"github.com/sdboyer/cuerious/internal/cuetxtar"
+)
+
+// func TestMain(m *testing.M) {
+// 	os.Exit(testscript.RunMain(m, map[string]func() int{}))
+// }
+
+func TestDumpExprTree(t *testing.T) {
+	test := cuetxtar.TxTarTest{
+		Root: "testdata",
+		Name: "exprtree",
+	}
+
+	test.Run(t, func(tc *cuetxtar.Test) {
+		ctx := cuecontext.New()
+		v := ctx.BuildInstance(tc.Instance())
+		if v.Err() != nil {
+			tc.Fatalf("errors in cue input: %s", v.Err())
+		}
+		iter, err := v.Fields(cue.All())
+		if err != nil {
+			tc.Fatal(err)
+		}
+
+		for iter.Next() {
+			w := tc.Writer(iter.Selector().String())
+			n := cuerious.ExprTree(iter.Value())
+			sb, err := format.Node(n.Self.Source())
+			if err != nil {
+				t.Fatalf("could not format node: %s", err)
+			}
+			fmt.Fprintf(w, "%s\n\n", string(sb))
+			fmt.Fprintf(w, "%s\n\n", n.String())
+		}
+	})
+}
--- go.mod
+++ go.mod
@@ -4,12 +4,21 @@ go 1.19
 
 require (
 	cuelang.org/go v0.5.0-beta.2
+	github.com/google/go-cmp v0.5.8
 	github.com/xlab/treeprint v1.1.0
+	golang.org/x/tools v0.1.12
 )
 
 require (
-	github.com/cockroachdb/apd/v2 v2.0.1 // indirect
+	github.com/cockroachdb/apd/v2 v2.0.2 // indirect
+	github.com/emicklei/proto v1.10.0 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/mpvl/unique v0.0.0-20150818121801-cbe035fff7de // indirect
 	github.com/pkg/errors v0.8.1 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	github.com/protocolbuffers/txtpbfmt v0.0.0-20220428173112-74888fd59c2b // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/text v0.3.8 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
